67b228bf0143c97346949592c95fe4a944a0f432
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-11 11:15:45 +0100

    add files necessary to publish

---
+++ README.md
@@ -0,0 +1,1 @@
+# Deploy Public API with Quota
--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.5, <2.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.0, <7.0"
-    }
-  }
-}
-
 # will have to taint
 resource "aws_api_gateway_deployment" "default" {
   rest_api_id = var.api_id
