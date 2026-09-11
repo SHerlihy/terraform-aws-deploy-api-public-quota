d3a777966e37755069f0b441de9dc9d8dce1481e
Steven Herlihy <steven_herlihy@yahoo.com>
2026-09-11 07:22:38 +0100

    add output descriptions

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "endpoint" {
+  description = "URL for endpoint."
   value = aws_api_gateway_stage.default.invoke_url
 }
 
 output "api_key" {
+  description = "API key subject to quota."
   value = nonsensitive(aws_api_gateway_api_key.default.value)
 }
